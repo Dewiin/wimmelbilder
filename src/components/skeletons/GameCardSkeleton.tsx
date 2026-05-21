2d0f9e32b382bdd3b664650e8a2f9423b0f9c7ec
Devin Xie <devinxie418@gmail.com>
2026-05-21 12:20:46 -0400

    feat: implement game card skeleton

--- src/components/skeletons/GameCardSkeleton.tsx
+++ src/components/skeletons/GameCardSkeleton.tsx
@@ -1,5 +1,25 @@
-
+import { Skeleton } from "@/components/ui/skeleton";
 
 export function GameCardSkeleton() {
-
+    return (
+        <Skeleton className="w-full md:h-165 h-110">
+            {/* <img
+                src={ map.imageUrl }
+                alt="Map cover"
+                className="relative z-20 aspect-square object-cover grayscale-25"
+                />
+            <CardHeader>
+                <CardTitle>{map.name.toLocaleUpperCase()}</CardTitle>
+                <CardDescription>{map.description}</CardDescription>
+            </CardHeader>
+            <CardFooter>
+                <Button 
+                    className="w-full cursor-pointer hover:bg-muted-foreground transition-colors duration-150 ease-in-out"
+                    onClick={() => handleNavigate()}
+                    >
+                    Play Game
+                </Button>
+            </CardFooter> */}
+        </Skeleton>
+    )
 }
