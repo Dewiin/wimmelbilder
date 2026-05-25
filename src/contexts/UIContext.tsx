201e20491dc0cedfb87951dcc826d8ea3ebbe3f9
Devin Xie <devinxie418@gmail.com>
2026-05-24 20:41:57 -0400

    feat: add toast contexts

--- src/contexts/UIContext.tsx
+++ src/contexts/UIContext.tsx
@@ -1,21 +1,37 @@
-import React, { createContext, useContext, useState } from "react";
+import type { TSonner } from "@/types/TSonner";
+import React, { createContext, useContext, useEffect, useState } from "react";
+import { toast } from "sonner";
 
 type UIContextProps = {
     isLoading: boolean
-    setIsLoading: React.Dispatch<React.SetStateAction<boolean>>
+    setIsLoading: React.Dispatch<React.SetStateAction<boolean>>,
+    sonner: TSonner|undefined,
+    setSonner: React.Dispatch<React.SetStateAction<TSonner|undefined>>
 }
 
 const UIContext = createContext<UIContextProps>({
     isLoading: false,
-    setIsLoading: () => {}
+    setIsLoading: () => {},
+    sonner: undefined,
+    setSonner: () => {}
 });
 
 export default function UIProvider({ children }: { children: React.ReactNode }) {
     const [ isLoading, setIsLoading ] = useState<boolean>(false);
+    const [ sonner, setSonner ] = useState<TSonner|undefined>();
+
+    useEffect(() => {
+        if(!sonner) return;
+        
+        if(sonner.variant === "success") toast.success(sonner.title, { description: sonner.description });
+        if(sonner.variant === "error") toast.error(sonner.title, { description: sonner.description });
+    }, [sonner]);
 
     const values = {
         isLoading, 
-        setIsLoading
+        setIsLoading,
+        sonner,
+        setSonner
     }
 
     return (
